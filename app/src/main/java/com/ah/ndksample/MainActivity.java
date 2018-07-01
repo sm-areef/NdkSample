233e5f083d417a45fb6b53405f0c152226e8edef
Madar Areef Hussain Shaik <sm.areef@gmail.com>
2018-07-01 15:28:16 +0800

    Ran code formatting
    Organizing imports on the project

--- app/src/main/java/com/ah/ndksample/MainActivity.java
+++ app/src/main/java/com/ah/ndksample/MainActivity.java
@@ -1,10 +1,8 @@
 package com.ah.ndksample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
